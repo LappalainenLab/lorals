ca6e6e433cc0c1b024be63a23fdfa1a06085771d
Paul Hoffman <mojaveazure@users.noreply.github.com>
2020-09-22 11:47:44 -0400

    Add documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,80 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+
+from pathlib import Path
+
+import sphinx_bootstrap_theme
+
+basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+sys.path.insert(0, basedir)
+
+# -- Project information -----------------------------------------------------
+
+project = 'LoRALS'
+copyright = '2020, Dafni Glinos, Paul Hoffman'
+author = 'Dafni Glinos, Paul Hoffman'
+
+# -- Options for Napoleon ----------------------------------------------------
+
+napoleon_google_docstring = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+# napoleon_include_init_with_doc = True
+
+# -- Autodoc configuration -----------------------------------------------------
+
+autoclass_content = 'class'
+
+autodoc_member_order = 'bysource'
+
+autodoc_default_flags = ['members']
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon"
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+# html_theme = 'alabaster'
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_theme_options = {
+    'bootswatch_theme': 'flatly',
+    'bootstrap_version': '3'
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
